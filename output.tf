7fe5ea22b6cf04d0726ec7c85087119faf52f51c
Narayan Parvatikar <narayan@Apples-MacBook-Pro.local>
2025-05-28 18:06:31 +0530

    Private Key Output added : sensitive

--- output.tf
+++ output.tf
@@ -17,3 +17,10 @@ output "eks_oidc_provider_arn" {
   description = "The OIDC provider ARN for the EKS cluster"
   value       = module.eks.oidc_provider_arn
 }
+
+# Output the private key for the user
+output "private_key_pem" {
+  description = "The private key in PEM format (only generated if enable_nfs is true and no existing PEM is provided)"
+  value       = var.enable_nfs && var.existing_pem == "" ? tls_private_key.ec2_key[0].private_key_pem : null
+  sensitive   = true
+}
